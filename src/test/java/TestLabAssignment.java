f73292f7f19cd8cc3744f40adb501c09b25e5577
DAVID-PC\David <oprutadavid@gmail.com>
2019-04-01 10:17:48 +0300

    added more tests

--- src/test/java/TestLabAssignment.java
+++ src/test/java/TestLabAssignment.java
@@ -1,12 +1,33 @@
+import Exceptions.ValidatorException;
+import Repository.XMLFileRepository.TemaLabXMLRepo;
+import Service.XMLFileService.TemaLabXMLService;
+import Validator.TemaLabValidator;
 import org.junit.Test;
 
+import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertTrue;
 
 public class TestLabAssignment {
 
+    private TemaLabValidator vt = new TemaLabValidator();
+    private TemaLabXMLRepo tmrepo = new TemaLabXMLRepo(vt, "D:\\vvss\\Project1\\src\\test\\java\\test2.xml");
+    private TemaLabXMLService tmsrv = new TemaLabXMLService(tmrepo);
 
     @Test
-    public void testAddLabAssignment(){
-        assertTrue(true);
+    public void testAddLabAssignment() {
+        String[] tema = {"1", "descriere", "5", "3"};
+        try {
+            tmsrv.add(tema);
+            assertEquals(1, tmrepo.getSize());
+        } catch (ValidatorException e) {
+            e.printStackTrace();
+            assertTrue(false);
+        }
+    }
+
+    @Test(expected = NumberFormatException.class)
+    public void testAddWithWrongParams() throws ValidatorException {
+        String[] tema = {"1", "descriere", "sdf", "3"};
+        tmsrv.add(tema);
     }
 }
--- src/test/java/TestStudent.java
+++ src/test/java/TestStudent.java
@@ -1,11 +1,26 @@
+import Exceptions.ValidatorException;
+import Repository.XMLFileRepository.StudentXMLRepo;
+import Service.XMLFileService.StudentXMLService;
+import Validator.StudentValidator;
 import org.junit.Test;
 
+import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertTrue;
 
 public class TestStudent {
 
     @Test
-    public void testAddStudent(){
-        assertTrue(true);
+    public void testAddStudent() {
+        StudentValidator vs = new StudentValidator();
+        StudentXMLRepo strepo = new StudentXMLRepo(vs, "D:\\vvss\\Project1\\src\\test\\java\\test.xml");
+        StudentXMLService stsrv = new StudentXMLService(strepo);
+        String[] student = {"1", "nume", "123", "asdfd", "asdfds"};
+        try {
+            stsrv.add(student);
+            assertEquals(1,strepo.getSize());
+        } catch (ValidatorException e) {
+            e.printStackTrace();
+            assertTrue(false);
+        }
     }
 }
